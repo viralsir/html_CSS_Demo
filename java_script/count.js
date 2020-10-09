8fd57b90515de6fd5f25d5b129a7ba8c78265c5d
viralsir <viralsir2018@gmail.com>
2020-10-09 10:28:10 +0530

    local storage and task app added.

--- java_script/count.js
+++ java_script/count.js
@@ -1,5 +1,10 @@
-  let count=0
+if(!localStorage.getItem('cnt'))
+{
+    localStorage.setItem('cnt',0);
+}
+
         function counter(){
+            let count=localStorage.getItem('cnt');
             count++;
 
             //alert(count);
@@ -8,8 +13,10 @@
                 alert(`count reach to  ${count}`);
             }
             document.querySelector("h1").innerHTML=count;
+            localStorage.setItem('cnt',count);
 
         }
         document.addEventListener('DOMContentLoaded',function(){
             document.querySelector("button").onclick=counter;
+            document.querySelector("h1").innerHTML=localStorage.getItem('cnt');
         })
